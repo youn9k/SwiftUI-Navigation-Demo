4ae2b554f5ab3c3eb6873f4ebda93055e6db719d
youn9k <ace_lephant@naver.com>
2026-01-15 19:19:58 +0900

    프로젝트 생성

---
+++ SwiftUI-Navigation-Demo/SwiftUI-Navigation-Demo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI-Navigation-Demo
+//
+//  Created by YoungK on 1/15/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI-Navigation-Demo/SwiftUI-Navigation-Demo/SwiftUI_Navigation_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Navigation_DemoApp.swift
+//  SwiftUI-Navigation-Demo
+//
+//  Created by YoungK on 1/15/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Navigation_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
